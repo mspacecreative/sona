708ec9d34deb221ff2821acc97fbb1e8cd99ec7e
Matt <matt@mspacecreative.com>
2020-08-18 10:11:01 -0300

    Bullets fix

--- wp-content/themes/sona/footer.php
+++ wp-content/themes/sona/footer.php
@@ -24,18 +24,18 @@
 								<div class="col col-lg-6 col-md-6 col-sm-6 col-xs-12">
 									<h3>Products &amp; Services</h3>
 									<div class="row">
-										<div class="col col-lg-6 col-md-12 col-sm-12 col-xs-12">
+										<div class="col col-lg-6 col-md-12 col-sm-12 col-xs-6">
 											<?php dynamic_sidebar('footer-area-1'); ?>
 										</div>
-										<div class="col col-lg-6 col-md-12 col-sm-12 col-xs-12">
+										<div class="col col-lg-6 col-md-12 col-sm-12 col-xs-6">
 											<?php dynamic_sidebar('footer-area-2'); ?>
 										</div>
 									</div>
 								</div>
-								<div class="col col-lg-2 col-md-3 col-sm-3 col-xs-12">
+								<div class="col col-lg-2 col-md-3 col-sm-3 col-xs-6">
 									<?php dynamic_sidebar('footer-area-3'); ?>
 								</div>
-								<div class="col col-lg-2 col-md-3 col-sm-3 col-xs-12">
+								<div class="col col-lg-2 col-md-3 col-sm-3 col-xs-6">
 									<?php dynamic_sidebar('footer-area-4'); ?>
 								</div>
 							</div>
