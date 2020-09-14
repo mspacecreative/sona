40dfdb1cb8c6718944cc68043d93df97760b7e6c
Matt <matt@mspacecreative.com>
2020-09-14 18:02:12 -0300

    c

--- wp-content/themes/sona/assets/js/scripts.js
+++ wp-content/themes/sona/assets/js/scripts.js
@@ -45,11 +45,7 @@
 		$(this).parent().next().fadeToggle();
 		
 		// FIXED CONTENT WHEN SOCIAL WINDOW IS OPEN
-		if ( $('.social-window').css('display') == 'block' ) {
-			$('.social-window').parent().parent().siblings('.wrap').css('position', 'fixed');
-		} else if ( $('.social-window').css('display') == 'none' ) {
-			$('.social-window').parent().parent().siblings('.wrap').css('position', 'relative');
-		}
+		$('body').toggleClass('fixed');
 	});
 	
 	// CHECK IF ABSOLUTE HEADING IS VISIBLE
