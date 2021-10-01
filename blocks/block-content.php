e0eb679035d3abe2fe1abe8751baafffdd334df7
Matt <matt@mspacecreative.com>
2021-10-01 14:31:33 -0300

    c

--- blocks/block-content.php
+++ blocks/block-content.php
@@ -10,6 +10,17 @@ $hide = get_field('hide_block');
 $overlayopacity = get_field('overlay_opacity');
 $bgposition = get_field('background_position');
 $blockvisibility = get_field('block_visibility');
+
+// CUSTOM ID
+$id = '' . $block['id'];
+if ( !empty($block['anchor']) ) {
+	$id = $block['anchor'];
+}
+// CUSTOM CLASS	
+$className = '';
+if( !empty($block['className']) ) {
+	$className .= ' ' . $block['className'];
+}
 	
 switch ($rowwidth) {
 	case '800':
@@ -23,6 +34,19 @@ switch ($rowwidth) {
 		break;
 	default:
 		$rowwidth = '';
+}
+switch ($bgposition) {
+	case 'top':
+		$bgposition = ' top center';
+		break;
+	case 'bottom':
+		$bgposition = ' bottom center';
+		break;
+	case 'center':
+		$bgposition = ' center center';
+		break;
+	default:
+		$bgposition = '';
 }
 switch ( $overlay ) {
 	case 'dark':
@@ -81,7 +105,7 @@ switch ( $bgcolor ) {
 		$shade = '';
 } ?>
 
-<section<?php if ( $blockanchor ): echo ' id="'; echo $blockanchor; echo '"'; endif; ?> class="content-section<?php if ( $align ): echo $align; endif; if ( $bg ): echo $bg; endif; if ( $bgimg ): echo ' section_has_bg_img'; endif; ?>"<?php if ( $bgimg ): echo ' style="background-image: url('; echo $bgimg; echo ');'; if ( $bgposition ): echo ' background-position: '; echo $bgposition; else: echo ' background-position: center'; endif; echo '"'; endif; if ( $blockvisibility == 0 ): echo 'style="display:none; opacity: 0.25;"'; endif; ?>>
+<section<?php if ( $id ): echo ' id="'; echo $id; echo '"'; endif; ?> class="content-section<?php if ( $className ): echo esc_attr($className); endif; if ( $align ): echo $align; endif; if ( $bg ): echo $bg; endif; if ( $bgimg ): echo ' section_has_bg_img'; endif; ?>"<?php if ( $bgimg ): echo ' style="background-image: url('; echo $bgimg; echo ');'; if ( $bgposition ): echo ' background-position: '; echo $bgposition; else: echo ' background-position: center'; endif; echo '"'; endif; if ( $blockvisibility == 0 ): echo 'style="display:none; opacity: 0.25;"'; endif; ?>>
 	
 	<?php if ( $overlay ): ?>
 	<div class="<?php if ( $overlay ): echo $overlay; endif; ?>" style="position: absolute; height: 100%; width: 100%; top: 0; left: 0; opacity: <?php if ( $overlayopacity ): echo $overlayopacity; else: echo '.75'; echo ';'; endif; ?>"></div>
