2f5d1f7bd0debfd5f5b8e36647f570f85cbf5cd2
Matt <matt@mspacecreative.com>
2021-10-01 18:12:11 -0300

    c

--- blocks/block-splash.php
+++ blocks/block-splash.php
@@ -1,5 +1,5 @@
 <!-- SPLASH SECTION -->
-<section class="splash">
+<section class="splash gold-bottom-border">
 	<?php
 	if ( have_rows('background_graphics') ):
 	while ( have_rows('background_graphics') ): the_row();
