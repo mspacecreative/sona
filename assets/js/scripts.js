9dd8cddcc684a543b9cfb6e78b6ac3bc1ca7d3ff
Matt <matt@mspacecreative.com>
2021-10-05 13:02:31 -0300

    c

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -32,8 +32,8 @@
 			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
 			if (target.length) {
 				$('html,body').animate({
-					scrollTop: target.offset().top - 50
-				}, 1000);
+					scrollTop: target.offset().top
+				}, 500);
 			    return false;
 			}
 		}
