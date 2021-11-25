86a122686ba4ee6dfad860e42c0017343d6815eb
Matt <matt@mspacecreative.com>
2021-11-25 08:39:08 -0400

    c

--- blocks/block-file-embed.php
+++ blocks/block-file-embed.php
@@ -20,12 +20,14 @@ if( !empty($block['className']) ) {
 	$className .= ' ' . $block['className'];
 } ?>
 
-<section<?php if ( $id ): echo ' id="'; echo $id; echo '"'; endif; ?> class="pdf-embed-block text-align-center bottom-padding<?php if ( $className ): echo esc_attr($className); endif; ?>">
+<section<?php if ( $id ): echo ' id="'; echo $id; echo '"'; endif; ?> class="pdf-embed-block text-align-center<?php if ( $className ): echo esc_attr($className); endif; ?>">
 	<?php if ( !empty($thumb) ): ?>
 	<div class="embed-thumbnail-container">
 		<img src="<?php echo $thumb['url'] ?>" alt="<?php if ($alt): echo $alt; else: echo 'Sona Nanotech'; endif; ?>">
 	</div>
 	<?php endif; ?>
 	<object class="wp-block-file__embed" data="<?php echo $file['url'] ?>" type="application/pdf" style="width:100%; height:<?php if ($height): echo $height; else: echo '600'; echo 'px;'; endif; ?>" aria-label="PDF embed"></object>
-	<a class="dark button" href="<?php echo $file['url'] ?>" target="_blank"><?php if ($label): echo __($label); else: echo __('View PDF'); endif; ?></a>
+	<div class="top-bottom-padding">
+		<a class="dark button" href="<?php echo $file['url'] ?>" target="_blank"><?php if ($label): echo __($label); else: echo __('View PDF'); endif; ?></a>
+	</div>
 </section>
